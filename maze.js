96445051e112e1eca77703a6a05bc7dbdb6a534b
matsoaresjc <matsoaresjc@gmail.com>
2016-10-13 18:35:23 -0700

    Exercise 3

--- maze.js
+++ maze.js
@@ -2,8 +2,11 @@
 
 window.onload = function()
 {
+	var flag = false; // Flag to control whether a wall has been hit.
 	var bound = document.querySelectorAll(".boundary");
 	
+	document.getElementById("start").addEventListener("mouseover",start);
+	document.getElementById("end").addEventListener("mouseover",end);
 	for (i = 0;i < bound.length;i++)
 		{
 			bound[i].addEventListener("mouseover",allborders);
@@ -21,5 +24,34 @@ function allborders()
 
 	for (i = 0;i < bound.length;i++)
 	{
+			bound[i].className ="boundary youlose";
 	}
+}
+
+
+function start()
+{
+
+var bound = document.querySelectorAll(".boundary");
+
+for (i = 0;i < bound.length;i++)
+	{
+			bound[i].classList.remove("you lose");
+	}
+
+}
+
+
+function end()
+{
+	flag = true;
+	
+	if(flag)
+	{
+		alert("You win!");
+	} else{
+		alert("Sorry you lost");
+	}
+
+
 }
